2e26339ea3a48b272dbd3b23a97e94c3ef597bd0
Hamza Dahmani <madebyhamzadahmani@gmail.com>
2021-12-17 16:08:01 +0100

    origin *

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const needle = require('needle');
 const url = require('url');
 const cors = require('cors');
 
-// app.use()
 app.prepare().then(() => {
   const API_BASE_URL = process.env.API_BASE_URL;
   const API_KEY_NAME = process.env.API_KEY_NAME;
@@ -18,34 +17,43 @@ app.prepare().then(() => {
   const APP_BASE_URL = process.env.APP_BASE_URL;
 
   const server = express();
+  server.use(
+    cors({
+      origin: '*',
+    })
+  );
   // server.use(
+  // cors({
+  //   origin: 'https://google.com',
+  // })
+  // );
+  // app.use(
   //   cors({
   //     origin: APP_BASE_URL,
   //   })
   // );
-  app.use(
-    cors({
-      origin: APP_BASE_URL,
-    })
-  );
 
-  server.get('/api/:stat', async (req, res) => {
-    try {
-      const params = new URLSearchParams({
-        ...url.parse(req.url, true).slashes,
-        ...url.parse(req.url, true).query,
-        [API_KEY_NAME]: API_KEY_VALUE,
-      });
-      const apiRes = await needle(
-        'get',
-        `${API_BASE_URL}/${req.params.stat}?${params}`
-      );
-      const data = apiRes.body;
-      res.status(200).json(data);
-    } catch (error) {
-      res.status(500).json({ error });
+  server.get(
+    '/api/:stat',
+
+    async (req, res) => {
+      try {
+        const params = new URLSearchParams({
+          ...url.parse(req.url, true).slashes,
+          ...url.parse(req.url, true).query,
+          [API_KEY_NAME]: API_KEY_VALUE,
+        });
+        const apiRes = await needle(
+          'get',
+          `${API_BASE_URL}/${req.params.stat}?${params}`
+        );
+        const data = apiRes.body;
+        res.status(200).json(data);
+      } catch (error) {
+        res.status(500).json({ error });
+      }
     }
-  });
+  );
 
   server.all('*', (req, res) => {
     return handle(req, res);
