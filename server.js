16c083c175b3df62917877dac82dd1b617a8bad5
Hamza Dahmani <madebyhamzadahmani@gmail.com>
2021-12-18 11:44:09 +0100

    changing port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const next = require('next');
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+// const port = parseInt(process.env.PORT, 10) || 3000;
+const port = process.env.PORT || 8080;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
